945167228d14de170da6a8052a6b7d85e9040eb3
mariana-silva-ifpar <silva.mariana1@escolar.ifrn.edu.br>
2025-07-01 14:20:13 -0300

    wip: adição de pesos e div visivel apenas quando existir média.

--- src/calcular-media/calcular-media.ts
+++ src/calcular-media/calcular-media.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calcular-media',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calcular-media.html',
   styleUrl: './calcular-media.css'
 })
@@ -10,7 +11,7 @@ export class CalcularMedia {
 media!: number;
 
   calcularMedia(n1: number, n2: number, n3: number, n4: number){
-    this.media = (n1 + n2 + n3 + n4) / 4;
+    this.media = (n1 *2 + n2 *2 + n3 *3 + n4 *3) / 10;
   }
 
 }
